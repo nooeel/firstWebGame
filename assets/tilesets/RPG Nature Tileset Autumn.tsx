edeb2a50d78d046eba372ebb382e50f52a0bb5f1
Noel Leon <noel.glaeser@gmail.com>
2022-10-24 22:52:54 +0200

    tilemap start

---
+++ assets/tilesets/RPG Nature Tileset Autumn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="RPG Nature Tileset Autumn" tilewidth="16" tileheight="16" tilecount="540" columns="30">
+ <image source="../raw/RPG Nature Tileset Autumn.png" width="480" height="288"/>
+</tileset>
