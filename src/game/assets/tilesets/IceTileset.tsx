40f09ca94f505338f8eb3dc036f1e8c643323e3f
Noel Leon <noel.glaeser@gmail.com>
2022-10-25 15:09:13 +0200

    anfang Tag 3 - geupdatet und Sortiert

--- src/game/assets/tilesets/IceTileset.tsx
+++ src/game/assets/tilesets/IceTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="IceTileset" tilewidth="16" tileheight="16" tilecount="720" columns="40">
- <image source="../raw/IceTileset.png" width="640" height="288"/>
+ <image source="raw/IceTileset.png" width="640" height="288"/>
 </tileset>
--- src/game/assets/tilesets/RPG Nature Tileset.tsx
+++ src/game/assets/tilesets/RPG Nature Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="RPG Nature Tileset" tilewidth="16" tileheight="16" tilecount="720" columns="40">
- <image source="../raw/RPG Nature Tileset.png" width="641" height="288"/>
+ <image source="raw/RPG Nature Tileset.png" width="641" height="288"/>
 </tileset>
