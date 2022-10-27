8d354a3be0dbaee4b11dd3a94d1902bb2213d86f
Noel Leon <noel.glaeser@gmail.com>
2022-10-27 20:15:38 +0200

    Assets hosten und Rendern, leon gerendert

--- src/game/lib/js/logic.js
+++ src/game/lib/js/logic.js
@@ -1,12 +1,29 @@
-const image = new Image();
+const backgroundImage = new Image();
+const playerImage = new Image();
 const canvas = document.querySelector('#main-background');
 const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-image.src = './assets/map/raw/FirstVillage.png';
+backgroundImage.src = './assets/map/raw/FirstVillage.png';
+playerImage.src = './assets/characters/leon.png';
 
-image.onload = function () {
-    ctx.drawImage(image, 0, -300);
-};
+backgroundImage.onload = function () {
+
+    ctx.drawImage(backgroundImage, -400, -600);
+
+    playerImage.onload = function () {
+        ctx.drawImage(
+            playerImage,
+            0,      // cropping x
+            0,      // crop y
+            playerImage.width / 4,  // single person
+            playerImage.height / 4,
+            canvas.width / 2 - 75,  // position
+            canvas.height / 2 + 60,
+            playerImage.width / 1.5,      
+            playerImage.height / 1.5
+        );
+    }
+}
